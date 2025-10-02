dd1ded773e3173b4eac603c46504d3293a137469
Tai Van <taivannho5a@gmail.com>
2025-10-02 07:47:16 +0700

    Add registration form and validation with Zod; update dependencies and environment configuration

--- client/src/app/(auth)/register/page.tsx
+++ client/src/app/(auth)/register/page.tsx
@@ -1,12 +1,21 @@
-import { ModeToggle } from '@/components/mode-toggle'
+import { ModeToggle } from "@/components/mode-toggle";
+import RegisterForm from "@/app/(auth)/register/register-form";
 
 const RegisterPage = () => {
+  console.log(
+    "process.env.NEXT_PUBLIC_API_ENDPOINT",
+    process.env.NEXT_PUBLIC_API_ENDPOINT
+  );
+
   return (
     <div>
       <ModeToggle />
-      Register page
+      <h1 className="text-xl font-semibold text-center">Register page</h1>
+      <div className="flex justify-center">
+        <RegisterForm />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;
---
+++ client/src/app/(auth)/register/register-form.tsx
@@ -0,0 +1,123 @@
+"use client";
+import { ModeToggle } from "@/components/mode-toggle";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import {
+  RegisterBody,
+  RegisterBodyType,
+} from "@/schemaValidations/auth.schema";
+import envConfig from "@/config";
+
+const RegisterForm = () => {
+  const form = useForm<RegisterBodyType>({
+    resolver: zodResolver(RegisterBody),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = async (values: RegisterBodyType) => {
+    const result = await fetch(
+      `${envConfig.NEXT_PUBLIC_API_ENDPOINT}/auth/register`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      }
+    ).then((res) => res.json());
+
+    console.log({ result });
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-2 w-full mx-4 max-w-[600px]"
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter your name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="Enter your email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="Enter your password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirm Password</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="Confirm your password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full !mt-10">
+          Register
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default RegisterForm;
